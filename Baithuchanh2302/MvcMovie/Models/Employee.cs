459c58536b3c70db787b18ee8dc23ef5da1014b3
KenNgx <ngxtuananh99@gmail.com>
2022-02-23 08:43:37 +0700

    Done

---
+++ Baithuchanh2302/MvcMovie/Models/Employee.cs
@@ -0,0 +1,16 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MvcMovie.models{
+    [Table("Employees")]
+    public class Employee{
+        [StringLength(50)]
+        [Key]
+        public string EmployeeID { get; set; }
+        [MinLength(3)]
+        [MaxLength(50)]
+        [Required]
+        public string EmployeeName { get; set; }
+    }
+}
---
+++ Baithuchanh2302/MvcMovie/Models/Person.cs
@@ -0,0 +1,16 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MvcMovie.models{
+    [Table("Persons")]
+    public class Person{
+        [StringLength(50)]
+        [Key]
+        public string PersonID { get; set; }
+        [MinLength(3)]
+        [MaxLength(50)]
+        [Required]
+        public string PersonName { get; set; }
+    }
+}
